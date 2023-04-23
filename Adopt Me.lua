3cc2f8910d83cc815e5489d9bdd10b31adb58974
FujiXDDDDDDDDD1 <71167709+FujiXDDDDDDDDD1@users.noreply.github.com>
2023-04-23 09:03:57 +0700

    Update Adopt Me.lua

--- Adopt Me.lua
+++ Adopt Me.lua
@@ -1,6 +1,6 @@
 local Config = {
-    WindowName = "Fu Kang Hub NextGeneration",
-    Color = Color3.fromRGB(0,194,255),
+    WindowName = "Fu Kang Hub V2",
+    Color = Color3.fromRGB(255,0,0),
     Keybind = Enum.KeyCode.RightControl
 }
 repeat wait() until game:IsLoaded()
